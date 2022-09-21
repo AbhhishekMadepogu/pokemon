005391968ce64b688c1db6fcf9bcbb67b92a46d8
Pat abhishek <patabhishek@Pats-MacBook-Air.local>
2022-09-21 01:03:56 -0400

    added logic to fetch the object details

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+const path = "https://pokeapi.co/api/v2/";
+const query = "pokemon?limit=20&offset=0";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const fetchPokemons = async () => {
+    let response = await fetch(`${path}${query}`);
+    response = await response.json();
+    console.log("Response", response.results);
+    await Promise.all(
+      response.results.map(async (e) => {
+        let resp = await fetch(e.url);
+        resp = await resp.json();
+        console.log(e.name);
+        resp.types.map((e) => {
+          console.log(e.type.name);
+        });
+        console.log(resp.sprites.front_default);
+      })
+    );
+  };
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
