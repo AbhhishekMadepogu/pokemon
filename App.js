577340d99f0a3134097987f1f4b9bf24ebbdc44f
Pat abhishek <patabhishek@Pats-MacBook-Air.local>
2022-09-21 13:04:14 -0400

    styling the components

--- App.js
+++ App.js
@@ -1,47 +1,95 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
 const path = "https://pokeapi.co/api/v2/";
 const query = "pokemon?limit=20&offset=0";
-
 export default function App() {
   const [pokemons, setPokemons] = useState([]);
-
+  const fetchPokemons = async () => {
+    let response = await fetch(`${path}${query}`);
+    response = await response.json();
+    await Promise.all(
+      response.results.map(async (e) => {
+        let resp = await fetch(e.url);
+        resp = await resp.json();
+        const name = e.name;
+        const types = [];
+        resp.types.map((e) => {
+          types.push(e.type.name);
+        });
+        const image_url = resp.sprites.front_default;
+        const pokemon = { name: name, types: types, image_url: image_url };
+        setPokemons((pokemons) => [...pokemons, pokemon]);
+      })
+    );
+  };
   useEffect(() => {
-    const fetchPokemons = async () => {
-      let response = await fetch(`${path}${query}`);
-      response = await response.json();
-      await Promise.all(
-        response.results.map(async (e) => {
-          let resp = await fetch(e.url);
-          resp = await resp.json();
-          const name = e.name;
-          const types = [];
-          resp.types.map((e) => {
-            types.push(e.type.name);
-          });
-          const image_url = resp.sprites.front_default;
-          const pokemon = { name: name, types: types, image_url: image_url };
-          console.log(pokemon);
-          setPokemons((pokemons) => [...pokemons, pokemon]);
-        })
-      );
-    };
     fetchPokemons();
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <FlatList
+        keyExtractor={(item) => item.key}
+        data={pokemons}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={styles.heading}>{item.name}</Text>
+              <View style={styles.imageCard}>
+                <ImageBackground
+                  style={styles.image}
+                  source={{ uri: item.image_url }}
+                ></ImageBackground>
+              </View>
+            </View>
+            <View style={styles.typeCard}>
+              {item.types.map((e) => {
+                return (
+                  <View style={{ backgroundColor: "#FFF" }}>
+                    <Text style={styles.types}>{e + " "}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        )}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  container: { flex: 1 },
+  card: {
+    backgroundColor: "#37796C",
+    height: 100,
+    width: "98%",
+    marginVertical: 10,
+    borderRadius: 20,
+    alignSelf: "center",
   },
+  image: { height: "100%", width: "100%", aspectRatio: 1 },
+  heading: {
+    color: "#FFF",
+    fontSize: 30,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  types: { color: "#000", marginLeft: 10, fontSize: 15 },
+  typeCard: { flexDirection: "row", marginTop: -40 },
+  imageCard: { height: 100, width: 100 },
 });
